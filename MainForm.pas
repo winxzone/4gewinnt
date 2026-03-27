f2e14918b9f0b8b2cdb5d015eff3c32089532cce
Illia Nevmyvannyi <159058512+winxzone@users.noreply.github.com>
2026-03-27 14:15:30 +0100

    Add CheckWin and IsBoardFull

---
+++ MainForm.pas
@@ -0,0 +1,294 @@
+unit MainForm;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    Button1: TButton;
+    PaintBox1: TPaintBox;
+    Button2: TButton;
+    LCurrentPlayer: TLabel;
+    procedure FormCreate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure PaintBox1Paint(Sender: TObject);
+    procedure PaintBox1MouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure Button2Click(Sender: TObject);
+  private
+    CurrentPlayer: Integer;
+    Board: array[0..6, 0..5] of Integer;
+    GameOver: Boolean;
+    procedure NewGame;
+    procedure CirclePaint(x, y: Integer);
+    procedure LCurrentPlayerUpdate;
+    procedure SwitchPlayer(var Player: Integer);
+
+    function CheckWin(x, y, Player : Integer) : Boolean;
+    function IsBoardFull : Boolean;
+
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Randomize;
+  NewGame;
+end;
+
+procedure TForm1.NewGame;
+var x, y: Integer;
+begin
+  GameOver := False;
+  PaintBox1.Invalidate;
+
+  for x := 0 to 6 do
+    for y := 0 to 5 do
+      Board[x, y] := 0;
+
+  // Settings CurrentPlayer + Random
+  CurrentPlayer := Random(2) + 1 ; // 1 oder 2
+  LCurrentPlayerUpdate;
+
+
+end;
+
+// Label Aktueller Spieler
+procedure TForm1.LCurrentPlayerUpdate();
+begin
+  LCurrentPlayer.Caption := 'Spieler ' + IntToStr(CurrentPlayer);
+  if CurrentPlayer = 1 then
+    LCurrentPlayer.Font.Color := clRed
+  else
+    LCurrentPlayer.Font.Color := clBlue;
+
+  LCurrentPlayer.Update;
+
+end;
+
+
+procedure TForm1.PaintBox1MouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+var _x, _y: Integer;
+begin
+  if GameOver then
+    Exit;
+  _x := (X div (PaintBox1.Width div 7));
+
+  for _y := 5 downto 0 do
+    begin
+      if Board[_x, _y] = 0 then
+        begin
+          Board[_x, _y] := CurrentPlayer;
+
+          PaintBox1.Invalidate;
+
+          if CheckWin(_x, _y, CurrentPlayer) then
+          begin
+            GameOver := True;
+            PaintBox1.Invalidate;
+            Exit;
+          end;
+
+          if (IsBoardFull) then
+          begin
+            ShowMessage('Niemand hat gewonnen.');
+            PaintBox1.Invalidate;
+            Exit;
+          end;
+
+          SwitchPlayer(CurrentPlayer);
+          LCurrentPlayerUpdate;
+
+          Exit;
+        end;
+//        ShowMessage('Column=' + IntToStr(Column) +', ' + 'Row=' + IntToStr(Row));
+    end;
+end;
+
+// var = kopie von CurrentPlayer
+procedure TForm1.SwitchPlayer(var Player: Integer);
+begin
+  if Player = 1 then
+  begin
+    Player := 2;
+
+  end
+  else
+    Player := 1;
+end;
+
+procedure TForm1.PaintBox1Paint(Sender: TObject);
+var x, y: Integer;
+begin
+    PaintBox1.Canvas.Brush.Color := clWhite;
+
+    for x := 0 to 6 do
+    for y := 0 to 5 do
+      CirclePaint(x, y);
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  NewGame;
+end;
+
+procedure TForm1.CirclePaint(x, y: Integer);
+var
+  radius, cx, cy: Integer;
+  CellWidth, CellHeight: Integer;
+  R: TRect;
+begin
+
+  CellWidth := PaintBox1.Width div 7;
+  CellHeight := PaintBox1.Height div 6;
+
+  cx := x * CellWidth + CellWidth div 2;
+  cy := y * CellHeight + CellHeight div 2;
+
+  radius := 25;
+
+  R := Rect(cx - radius, cy - radius, cx + radius, cy + radius);
+  PaintBox1.Canvas.Brush.Color := clWhite;
+
+  case Board[x, y] of
+    0: PaintBox1.Canvas.Brush.Color := clWhite;
+    1: PaintBox1.Canvas.Brush.Color := clRed;
+    2: PaintBox1.Canvas.Brush.Color := clBlue;
+  end;
+
+  PaintBox1.Canvas.Pen.Color := clBlack;
+  PaintBox1.Canvas.Pen.Width := 2;
+  PaintBox1.Canvas.Ellipse(R);
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  ShowMessage('Du hast Beenden Button geklickt. Niemand hat gewonnen.');
+end;
+
+function TForm1.CheckWin(x, y, Player : Integer) : Boolean;
+var i, j, counterH, counterV, counterD1, counterD2: Integer;
+begin
+  Result := False;
+  counterH := 1;
+  counterV := 1;
+  counterD1 := 1;
+  counterD2 := 1;
+
+  if (x >= 0) and (x <= 6) and (y >= 0) and (y <= 5) then
+  begin
+
+    // Horizontal
+    i := x - 1;
+    while (i >= 0) and (Board[i, y] = Player) do
+    begin
+       counterH := counterH + 1;
+       i := i - 1;
+    end;
+
+    i := x + 1;
+    while (i <= 6) and (Board[i, y] = Player) do
+    begin
+      counterH := counterH + 1;
+      i := i + 1;
+    end;
+
+    // Vertikal
+    j := y - 1;
+    while (j >= 0) and (Board[x, j] = Player) do
+    begin
+       counterV := counterV + 1;
+       j := j - 1;
+    end;
+
+    j := y + 1;
+    while (j <= 5) and (Board[x, j] = Player) do
+    begin
+      counterV := counterV + 1;
+      j := j + 1;
+    end;
+
+    // X
+    i := x - 1;
+    j := y - 1;
+    while (i >= 0) and (j >= 0) and (Board[i, j] = Player) do
+    begin
+      counterD1 := counterD1 + 1;
+      i := i - 1;
+      j := j - 1;
+    end;
+
+    i := x + 1;
+    j := y + 1;
+    while (j <= 5) and (i <= 6) and (Board[i, j] = Player) do
+    begin
+      counterD1 := counterD1 + 1;
+      i := i + 1;
+      j := j + 1;
+    end;
+
+//  -------------------------------------------------
+//  von links nach rechts, von rechts nach links
+    i := x + 1;
+    j := y - 1;
+    while (i <= 6) and (j >= 0) and (Board[i, j] = Player) do
+    begin
+      counterD2 := counterD2 + 1;
+      i := i + 1;
+      j := j - 1;
+    end;
+
+    i := x - 1;
+    j := y + 1;
+    while (i >= 0) and (j <= 5) and (Board[i, j] = Player) do
+    begin
+      counterD2 := counterD2 + 1;
+      i := i - 1;
+      j := j + 1;
+    end;
+
+     OutputDebugString(PChar('CounterH=' + IntToStr(counterH) + ' ' + 'CounterV=' + IntToStr(counterV) + ' ' + 'CounterD1=' + IntToStr(counterD1) + ' ' + 'CounterD2=' + IntToStr(counterD2)));
+
+//    ShowMessage('CounterH=' + IntToStr(counterH) + ' ' + 'CounterV=' + IntToStr(counterV) + ' ' + 'CounterD1=' + IntToStr(counterD1) + ' ' + 'CounterD2=' + IntToStr(counterD2));
+    if (counterH >= 4) or (counterV >= 4) or (counterD1 >= 4) or (counterD2 >= 4) then
+    begin
+      Result := True;
+      ShowMessage('Spieler ' + IntToStr(Player) + ' hat gewonnen!!!');
+      Exit;
+    end;
+
+  end;
+
+
+end;
+
+function TForm1.IsBoardFull : Boolean;
+var x : Integer;
+begin
+  Result := True;
+  for x := 0 to 6 do
+  begin
+    if Board[x, 0] = 0 then
+    begin
+      Result := False;
+      Exit;
+    end;
+  end;
+
+end;
+
+end.
